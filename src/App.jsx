48f4ae35243b99ff8ae3daf687246c65067dda16
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-13 00:21:51 +0100

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from './components/Navigation'
+import Hero from './components/Hero'
+import Features from './components/Features'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950">
+      <Navigation />
+      <Hero />
+      <Features />
+    </div>
+  )
+}
+
+export default App
